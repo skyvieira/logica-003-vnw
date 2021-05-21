e65b1761aa65b75d5b304862e0502a299102e0db
Ana Vieira <vieira.liviaaa@gmail.com>
2021-05-21 11:54:03 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+for (let num1 = 1; num1 < 11; num1++) {
+    console.log(num1)
+}
+
+let num2 = 10
+while (num2 > 0) {
+    console.log(num2)
+    num2--
+}
+
+let num3 = 1
+while (num3 < 100) {
+    console.log(num3)
+    num3+=2
+}
+
+let num4 = 0
+while (num4 <= 100) {
+    console.log(num4)
+    num4+=2
+}
